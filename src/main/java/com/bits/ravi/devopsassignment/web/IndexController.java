9332f03fefaadf17b44e0735338eae2a9a1cee72
Ravi Kant Pandey <ravikantvicky@gmail.com>
2022-11-13 20:54:46 +0530

    Dev branch

--- src/main/java/com/bits/ravi/devopsassignment/web/IndexController.java
+++ src/main/java/com/bits/ravi/devopsassignment/web/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping
     public String index() {
-        return "Hello DevOps World !";
+        return "Hello DevOps World !" +
+                "<br/>" +
+                "Added in development branch !";
     }
 }
